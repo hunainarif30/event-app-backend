a35d71e896edac5392f33ab1f668d11520fa9311
Muhammad Areeb <58852461+mareebbaig@users.noreply.github.com>
2023-01-16 17:31:47 +0500

    summary

--- apis/event_api.js
+++ apis/event_api.js
@@ -1,10 +1,11 @@
 const axios = require("axios");
-require('dotenv').config();
-const headers = require("../config/debug.json")
+require("dotenv").config();
+const headers = require("../config/debug.json");
+
 async function destinationInfo() {
   const response = await axios({
     method: "get",
-    url: `${process.env.BASE_URL}/v1/taxonomy/destinations`,
+    url: `https://api.viator.com/partner/v1/taxonomy/destinations`,
     headers: {
       "exp-api-key": "4d1960fb-a0b2-4ed5-8194-811ef09a9db8",
       Accept: "application/json;version=2.0",
@@ -18,7 +19,7 @@ async function destinationInfo() {
 async function eventInfo(start, destinationId) {
   const response = await axios({
     method: "post",
-    url: `${process.env.BASE_URL}/products/search`,
+    url: `https://api.viator.com/partner/products/search`,
     headers: {
       "exp-api-key": "4d1960fb-a0b2-4ed5-8194-811ef09a9db8",
       Accept: "application/json;version=2.0",
--- services/services.js
+++ services/services.js
@@ -1,10 +1,7 @@
 //const createTable = require("../queries/create_queries");
 const db = require("../adapter/pgsql");
+const pgp = require("pg-promise")();
 const { v4: uuidv4 } = require("uuid");
-async function createDestinations() {
-  const result = await db.query();
-  return result;
-}
 
 async function insert_destinations(id, destination_city) {
   const result = await db.query(
@@ -16,6 +13,7 @@ async function insert_destinations(id, destination_city) {
 
 async function create_partitions(destination_id, destinationName) {
   Tablename = destinationName.replace(/\s/g, "");
+  Tablename = Tablename.replace(/-/g, "");
   const result = await db.query(
     `CREATE TABLE ${Tablename} PARTITION OF events FOR VALUES IN (${destination_id})`,
   );
@@ -24,64 +22,54 @@ async function create_partitions(destination_id, destinationName) {
 }
 
 async function create_event_table() {
-  db.tx((t) => {
-    // creating a sequence of transaction queries:
-    const q0 = t.none(
+  const result = await db.tx(async (t) => {
+    const q0 = await t.none(
       "CREATE TABLE destinations (destination_id VARCHAR PRIMARY KEY , city VARCHAR )",
     );
-    const q1 = t.none(
+    const q1 = await t.none(
       "CREATE TABLE events (event_id VARCHAR  , destination_id varchar , title VARCHAR, description TEXT, url TEXT  , PRIMARY KEY (event_id , destination_id)) PARTITION BY LIST(destination_id)",
     );
-    const q2 = t.none(
+    const q2 = await t.none(
       "CREATE TABLE images (image_id VARCHAR PRIMARY KEY, image_url TEXT, event_id VARCHAR, destination_id varchar , FOREIGN KEY (event_id , destination_id)REFERENCES events  (event_id , destination_id))",
     );
-    const q3 = t.none(
+    const q3 = await t.none(
       "CREATE TABLE review (review_id VARCHAR PRIMARY KEY, average_rating NUMERIC(2,1), provider VARCHAR, event_id VARCHAR , destination_id varchar , FOREIGN KEY (event_id , destination_id)REFERENCES events  (event_id , destination_id))",
     );
-    const q4 = t.none(
+    const q4 = await t.none(
       "CREATE TABLE tag_info (tag_id VARCHAR PRIMARY KEY , tag_name VARCHAR  )",
     );
-    const q5 = t.none(
+    const q5 = await t.none(
       "CREATE TABLE tags ( event_id VARCHAR ,tag_id VARCHAR, destination_id varchar , FOREIGN KEY (event_id , destination_id)REFERENCES events  (event_id , destination_id), FOREIGN KEY (tag_id) REFERENCES tag_info (tag_id)  )",
     );
-    const q6 = t.none(
+    const q6 = await t.none(
       "CREATE TABLE currency (currency_id VARCHAR PRIMARY KEY,currency VARCHAR )",
     );
-    const q7 = t.none(
+    const q7 = await t.none(
       "CREATE TABLE price (event_id VARCHAR,currency  VARCHAR, destination_id varchar , price NUMERIC(10,5),  discount NUMERIC (10,5), FOREIGN KEY (event_id , destination_id) REFERENCES events  (event_id , destination_id))",
     );
-    const q8 = t.none(
+    const q8 = await t.none(
       "CREATE TABLE over_all_ranting (event_id VARCHAR,rating NUMERIC(5,4), destination_id varchar , FOREIGN KEY (event_id , destination_id)REFERENCES events  (event_id , destination_id) )",
     );
-    return t.batch([q1, q2, q3, q4, q5, q6, q7, q8]); // all of the queries are to be resolved;
-  })
-    .then((data) => {
-      console.log("sucess"); // commit
-    })
-    .catch((error) => {
-      // failure, ROLLBACK was executed
-    });
+    return t.batch([q0, q1, q2, q3, q4, q5, q6, q7, q8]); // all of the queries are to be resolved;
+  });
+  return result;
 }
 
 async function DropAll() {
-  db.tx((t) => {
-    const q8 = t.none("drop table over_all_ranting");
-    const q7 = t.none("drop table price");
-    const q6 = t.none("drop table currency");
-    const q5 = t.none("drop table tags");
-    const q4 = t.none("drop table tag_info");
-    const q3 = t.none("drop table review");
-    const q2 = t.none("drop table images");
-    const q1 = t.none("drop table events");
-    const q0 = t.none("drop table destinations");
-    return t.batch([q1, q2, q3, q4, q5, q6, q7, q8]); // all of the queries are to be resolved;
-  })
-    .then((data) => {
-      console.log("sucess"); // commit
-    })
-    .catch((error) => {
-      // failure, ROLLBACK was executed
-    });
+  const result = await db.tx(async (t) => {
+    const q8 = await t.none("drop table over_all_ranting");
+    const q7 = await t.none("drop table price");
+    const q6 = await t.none("drop table currency");
+    const q5 = await t.none("drop table tags");
+    const q4 = await t.none("drop table tag_info");
+    const q3 = await t.none("drop table review");
+    const q2 = await t.none("drop table images");
+    const q1 = await t.none("drop table events");
+    const q0 = await t.none("drop table destinations");
+
+    return t.batch([q0, q1, q2, q3, q4, q5, q6, q7, q8]); // all of the queries are to be resolved;
+  });
+  return result;
 }
 
 async function createCurrency() {
@@ -132,7 +120,6 @@ async function insert_to_db(data, destination_id) {
 }
 
 module.exports = {
-  createDestinations,
   insert_destinations,
   create_event_table,
   create_partitions,
