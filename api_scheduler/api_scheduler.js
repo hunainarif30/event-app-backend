bbd0ab43025b2c4f71ca8aebb52995761c8b11f3
Hunain Arif <75844785+hunainarif30@users.noreply.github.com>
2023-01-18 16:41:22 +0500

    updated

--- api_scheduler/api_scheduler.js
+++ api_scheduler/api_scheduler.js
@@ -1,8 +1,8 @@
-const cron = require('node-cron');
+const cron = require("node-cron");
 const destinationApi = require("../controller/controller");
 
 // const fetchDestnations = cron.schedule('0 0 * * *', () => {
-    const fetchDestinations = cron.schedule('* * * * *', () => {
+const fetchDestinations = cron.schedule("* * * * *", () => {
   destinationApi();
 });
 
