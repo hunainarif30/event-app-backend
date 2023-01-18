b7a1f8dc0dc0ce8e24e278ffe476e11e344ef926
Hunain Arif <75844785+hunainarif30@users.noreply.github.com>
2023-01-18 16:40:32 +0500

    bug fixed

--- api_scheduler/api_scheduler.js
+++ api_scheduler/api_scheduler.js
@@ -1,5 +1,5 @@
 const cron = require('node-cron');
-const destinationApi = require("./controller/controller");
+const destinationApi = require("../controller/controller");
 
 // const fetchDestnations = cron.schedule('0 0 * * *', () => {
     const fetchDestinations = cron.schedule('* * * * *', () => {
